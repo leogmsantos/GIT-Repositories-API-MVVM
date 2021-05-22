c60667583d501d4249dfd7be0b3c6e57a33c42fe
Leo <leonardo.santos@foton.la>
2021-05-22 18:58:24 -0300

    Adição de detalhes aos repositórios

--- GitRepositories/app/src/main/java/br/com/leogmsantos/gitrepositories/model/GITRepository.kt
+++ GitRepositories/app/src/main/java/br/com/leogmsantos/gitrepositories/model/GITRepository.kt
@@ -2,6 +2,7 @@ package br.com.leogmsantos.gitrepositories.model
 
 import androidx.room.*
 import br.com.leogmsantos.gitrepositories.base.DTO
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Entity
@@ -15,5 +16,15 @@ data class GITRepository (
     val description: String,
     val owner: GITRepositoryOwner,
     val forks: Int,
-    val watchers: Int
+    val watchers: Int,
+    @SerializedName("clone_url")
+    val cloneUrl: String,
+    @ColumnInfo(defaultValue = "")
+    val language: String,
+    @SerializedName("created_at")
+    @ColumnInfo(defaultValue = "")
+    val createdAt: String,
+    @SerializedName("updated_at")
+    @ColumnInfo(defaultValue = "")
+    val lastUpdate: String
 ): DTO
--- GitRepositories/app/src/main/java/br/com/leogmsantos/gitrepositories/view/adapter/RepositoryViewHolder.kt
+++ GitRepositories/app/src/main/java/br/com/leogmsantos/gitrepositories/view/adapter/RepositoryViewHolder.kt
@@ -1,13 +1,20 @@
 package br.com.leogmsantos.gitrepositories.view.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import br.com.leogmsantos.gitrepositories.databinding.ItemGitRepositoryBinding
 import br.com.leogmsantos.gitrepositories.model.GITRepository
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RepositoryViewHolder (private val binding: ItemGitRepositoryBinding) : RecyclerView.ViewHolder(binding.root){
 
@@ -20,15 +27,45 @@ class RepositoryViewHolder (private val binding: ItemGitRepositoryBinding) : Rec
     }
 
     fun bind(repository: GITRepository, context: Context){
-        binding.tvItemTitle.text = repository.name.toUpperCase()
+        binding.tvItemTitle.text = repository.name.toUpperCase(Locale.ROOT)
         binding.tvItemDescription.text = repository.description
         binding.tvForkCount.text = repository.forks.toString()
         binding.tvFavouriteCount.text = repository.watchers.toString()
-        binding.tvUsername.text = repository.owner?.login
+        binding.tvUsername.text = repository.owner.login
         Glide.with(context)
-            .load(repository.owner?.avatarUrl)
+            .load(repository.owner.avatarUrl)
             .apply(RequestOptions.centerCropTransform())
             .into(binding.ivAvatar)
+
+        binding.tvLanguageValue.text = repository.language
+        binding.tvCreatedAtValue.text = convertData(repository.createdAt)
+        binding.tvLastUpdateValue.text = convertData(repository.lastUpdate)
+
+        binding.ivExpand.setOnClickListener {
+            binding.ivExpand.animate().rotationBy(180F).setDuration(300).start()
+            binding.detailsContainer.visibility = if (binding.detailsContainer.visibility == View.GONE) View.VISIBLE else View.GONE
+        }
+
+        binding.ivClone.setOnClickListener {
+            context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(repository.cloneUrl)))
+        }
     }
 
+    private fun convertData(data: String): String? {
+        val ts = data.split("T").toTypedArray()
+        val date = ts[0]
+        var dataFormatada = ""
+        val fromUser = SimpleDateFormat("dd/MM/yyyy")
+        val myFormat = SimpleDateFormat("yyyy-MM-dd")
+        try {
+            dataFormatada = fromUser.format(myFormat.parse(date))
+        } catch (e: ParseException) {
+            Log.d(
+                this.javaClass.name,
+                e.message,
+                e
+            )
+        }
+        return dataFormatada
+    }
 }
